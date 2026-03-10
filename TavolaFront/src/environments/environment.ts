e9dafbdf3573e978e1dca5dce1941b1d159a61a1
SergioS0ares <sergyn200295@gmail.com>
2026-03-09 23:23:36 -0300

    Refatorando o codigo do login e atualizando para a versão 21 do angular

--- TavolaFront/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8080', // Adjust this URL to match your backend API
-  recaptchaSiteKey: '6Ld3jIIsAAAAABndFXtLD84xSp7Terd6A3EMTPDB'
-}; 
